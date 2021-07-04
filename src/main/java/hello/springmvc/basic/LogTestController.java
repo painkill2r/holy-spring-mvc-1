68d1395bcfa9c563cfd7a659cfcbd41984ed5f7d
wdEffort <yoman3011@gmail.com>
2021-07-04 19:56:23 +0900

    로깅 설정 - @Slf4j 설정 사용

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,5 +1,6 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
  * 그래서 View를 찾고 View가 렌더링 된다.
  * 하지만 @RestController는 반환 값으로 View를 찾는 것이 아니라, HTTP 메시지 바디에 바로 입력한다.
  */
+@Slf4j
 @RestController
 public class LogTestController {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    //@Slf4j 어노테이션을 사용하면 직접 선언하지 않아도 됨.
+    //private final Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/log-test")
     public String logTest() {
         String name = "spring";
 
         //System.out.println("name = " + name);
-        //로그 레벨 설정에 따라 출력됨
+        //로그 레벨 설정에 따라 출력 됨.
+        //log.trace("[trace] name = " + name); // 연산이 발생하므로 권장하지 않음.
         log.trace("[trace] name = {}", name);
         log.debug("[debug] name = {}", name);
         log.info("[info] name = {}", name);
